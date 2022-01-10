6c6c1e88264853f09e0cdba2a1b3e5ee91783451
Vitaliy Kudryk <kudryk.v@protonmail.com>
2022-01-10 19:37:49 +0200

    use repositories

--- src/command-repositories.tsx
+++ src/command-repositories.tsx
@@ -1,107 +1,19 @@
 // noinspection JSUnusedGlobalSymbols
 
-import {
-  getLocalStorageItem,
-  LocalStorageValue,
-  setLocalStorageItem,
-  showToast,
-  ToastStyle
-} from "@raycast/api";
-import { useEffect, useState } from "react";
-
-import { getRepos } from "./octokit-interations";
 import { ListRepositories } from "./ListRepositories";
-import { RepositoryObject } from "./types";
-
-const STORAGE_FULL_NAMES = "cached-full-names";
-const STORAGE_STARRED = "starred-repos-full-names";
+import { useRepositories } from "./useRepositories";
 
 const Command = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [starred, setStarred] = useState<RepositoryObject[]>([]);
-  const [rest, setRest] = useState<RepositoryObject[]>([]);
-
-  const onRefresh = () => Promise.resolve()
-    .then(() => setIsLoading(true))
-    .then(pullRepos)
-    .then(cacheRepos)
-    .then(() => setIsLoading(false))
-    .catch(showError);
-
-  useEffect(() => {
-    Promise.all([
-      getLocalStorageItem(STORAGE_FULL_NAMES)
-        .then(parseSerializedRepos)
-        .then(cacheIfNotYetCached),
-      getLocalStorageItem(STORAGE_STARRED)
-        .then(parseSerializedStars)
-    ])
-      .then(([repos, stars]) =>
-        repos.reduce(({ starred, rest }, curr) => {
-          stars.find(star => star === curr.full_name)
-            ? starred.push(curr)
-            : rest.push(curr);
-
-          return { starred, rest };
-        }, { starred: [], rest: [] } as { starred: RepositoryObject[], rest: RepositoryObject[] }))
-      .then(({starred, rest}) => {
-        setStarred(starred);
-        setRest(rest);
-        setIsLoading(false);
-      })
-  }, []);
+  const { isLoading, starred, rest } = useRepositories();
 
   return <ListRepositories
     isLoading={isLoading}
     starred={starred}
     rest={rest}
-    onRefresh={onRefresh}
+    onRefresh={() => {
+    }}
   />;
 };
 
 export default Command;
 
-
-const pullRepos = () => getRepos()
-  .then(list => ({ list, cache: false }));
-
-
-const parseSerializedRepos = (serialized: LocalStorageValue | undefined) => {
-  if (!serialized || typeof serialized !== "string") {
-    return pullRepos();
-  }
-
-  const list = JSON.parse(serialized) as RepositoryObject[];
-
-  if (!list || !list.length || list.length === 0) {
-    return pullRepos();
-  }
-
-  return Promise.resolve({ list, cache: true });
-};
-
-
-const parseSerializedStars = (serialized: LocalStorageValue | undefined): string[] => {
-  if (!serialized || typeof serialized !== "string") {
-    return [];
-  }
-
-  return JSON.parse(serialized) || [];
-};
-
-
-const cacheRepos = ({ list }: { list: RepositoryObject[] }) =>
-  setLocalStorageItem(STORAGE_FULL_NAMES, JSON.stringify(list))
-    .then(() => list);
-
-
-const cacheIfNotYetCached = ({
-                               list,
-                               cache
-                             }: { list: RepositoryObject[], cache: boolean }): Promise<RepositoryObject[]> =>
-  cache
-    ? Promise.resolve(list)
-    : cacheRepos({ list });
-
-
-const showError = (e: Error) => showToast(ToastStyle.Failure, e.message);
---
+++ src/useRepositories.ts
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import { RepositoryObject } from "./types";
+import { getLocalStorageItem, LocalStorageValue, setLocalStorageItem } from "@raycast/api";
+import { getRepos } from "./octokit-interations";
+
+const STORAGE_FULL_NAMES = "cached-full-names";
+const STORAGE_STARRED = "starred-repos-full-names";
+
+export const useRepositories = () => {
+  const [{ starred, rest, isLoading }, setState] = useState({
+    starred: [] as RepositoryObject[],
+    rest: [] as RepositoryObject[],
+    isLoading: true
+  });
+
+  useEffect(() => {
+    Promise.all([
+      getLocalStorageItem(STORAGE_FULL_NAMES)
+        .then(serializedRepos)
+        .then(pullOnEmptyCache)
+        .then(cacheIfNotYetCached),
+      getLocalStorageItem(STORAGE_STARRED)
+        .then(parseSerializedStars)
+    ])
+      .then(groupByStars)
+      .then(({ starred, rest }) => setState({ starred, rest, isLoading: false }));
+  }, []);
+
+  return { isLoading, starred, rest };
+};
+
+
+const groupByStars = ([repos, stars]: [repos: RepositoryObject[], stars: string[]]) =>
+  repos.reduce(({ starred, rest }, curr) => {
+    stars.find(star => star === curr.full_name)
+      ? starred.push(curr)
+      : rest.push(curr);
+
+    return { starred, rest };
+  }, { starred: [], rest: [] } as { starred: RepositoryObject[], rest: RepositoryObject[] });
+
+
+const serializedRepos = (serialized: LocalStorageValue | undefined) => {
+  if (!serialized || typeof serialized !== "string" || serialized.length < 3) {
+    return { list: [], cache: false };
+  }
+
+  const list = JSON.parse(serialized) as RepositoryObject[];
+  if (!list || !list.length || list.length === 0) {
+    return { list: [], cache: false };
+  }
+
+  return { list, cache: true };
+};
+
+
+const pullOnEmptyCache = ({ list, cache }: { list: RepositoryObject[], cache: boolean }) =>
+  cache
+    ? Promise.resolve({ list, cache })
+    : getRepos().then(list => ({ list, cache: false }));
+
+
+const parseSerializedStars = (serialized: LocalStorageValue | undefined): string[] => {
+  if (!serialized || typeof serialized !== "string") {
+    return [];
+  }
+
+  return JSON.parse(serialized) || [];
+};
+
+
+const cacheIfNotYetCached = ({ list, cache }: { list: RepositoryObject[], cache: boolean }) =>
+  cache
+    ? Promise.resolve(list)
+    : setLocalStorageItem(STORAGE_FULL_NAMES, JSON.stringify(list)).then(() => list);
