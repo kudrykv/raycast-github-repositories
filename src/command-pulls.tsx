cdc7bc6b146dc26d2f43f128acaaf6517e799f56
Vitaliy Kudryk <kudryk.v@protonmail.com>
2022-01-10 17:01:04 +0200

    future cmd

---
+++ src/command-pulls.tsx
@@ -0,0 +1,8 @@
+import { List } from "@raycast/api";
+
+// noinspection JSUnusedGlobalSymbols
+export default function CommandPulls() {
+  return <List>
+    <List.Item title={"hello world"} />
+  </List>
+}
